db13bc39a51f68e7fbbc73a8b1b7dbd2c9b4bf66
fzuppa <fzuppa@gmail.com>
2012-02-29 12:30:52 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineTestProject::Application.config.secret_token = '8b98891d93d24525b326ee353fb56c3968151c569bfaed7a81778005f9988a391e21c246798c23b25953d8dd31333e33b3807d1b3bc978cd67df401a071cb82d'
---
+++ spec/javascripts/foo_spec.js.coffee
@@ -0,0 +1,4 @@
+describe "Foo", ->
+  it "it is not bar", ->
+    v = new Foo()
+    expect(v.bar()).toEqual(false)
